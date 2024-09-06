eb02104696df413fba21756b7cd1c63ab690e824
Faisol <faisol.wr@gmail.com>
2024-09-06 16:56:26 +0700

    [Create] docker, index.php to init

---
+++ sql-injection-lab/index.php
@@ -0,0 +1,25 @@
+<?php
+$servername = "db";
+$username = "root";
+$password = "password";
+$dbname = "testdb";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+if(isset($_GET['id'])) {
+    $id = $_GET['id'];
+    $sql = "SELECT * FROM users WHERE id = $id";
+    $result = $conn->query($sql);
+    if ($result->num_rows > 0) {
+        while($row = $result->fetch_assoc()) {
+            echo "id: " . $row["id"]. " - Name: " . $row["name"]. "<br>";
+        }
+    } else {
+        echo "0 results";
+    }
+}
+$conn->close();
+?>
---
+++ sql-injection-lab/init.sql
@@ -0,0 +1,3 @@
+USE testdb;
+CREATE TABLE users (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(50));
+INSERT INTO users (name) VALUES ('Alice'), ('Bob'), ('Charlie');
